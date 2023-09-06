f9f2bee1c866eeebf9acc8c47ce7b3efe94ad04f
arnavr15 <arnav.ranjan@likeminds.community>
2023-09-06 15:25:26 +0530

    added submodule

--- app/src/main/java/com/likeminds/parentrepository/MainActivity.kt
+++ app/src/main/java/com/likeminds/parentrepository/MainActivity.kt
@@ -10,22 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.likeminds.mylibrary.MyLibrary
 import com.likeminds.parentrepository.ui.theme.ParentRepositoryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ParentRepositoryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        MyLibrary.init("Hello - 1")
     }
 }
 
